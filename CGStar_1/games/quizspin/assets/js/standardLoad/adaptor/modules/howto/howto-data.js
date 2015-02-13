5359ec0f28db1658bca187819b556bdd2e004fd5
vandanaptotem <vandana.ptotem@gmail.com>
2015-02-13 13:32:38 +0530

    changes inmiliar

---
+++ CGStar_1/games/quizspin/assets/js/standardLoad/adaptor/modules/howto/howto-data.js
@@ -0,0 +1,9 @@
+
+window = {};
+
+window.data = [
+    {loc: "diva", description: "<span>This is Div A</span>", sequence: 4},
+    {loc: "divb", description: "<span>This is Div B This is Div B This is Div B This is Div B This is Div B This is Div B This is Div B This is Div B This is Div B This is Div B This is Div B This is Div B </span>", sequence: 2},
+    {loc: "divc", description: "<span>This is Div C</span>", sequence: 3},
+    {loc: "divb", description: "<img src='img/arrow.png'/>", sequence: 10}
+]
